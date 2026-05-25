bdb2b06d50ffe6a383b366ff88b559a5ed5b8f6a
KRISHNENDU PATRA <104295333+kririntu@users.noreply.github.com>
2026-05-25 11:50:48 +0530

    Clean up imports in rag.py
    Removed unused imports from rag.py.

--- rag.py
+++ rag.py
@@ -1,7 +1,5 @@
 import os
 from langchain_classic.memory import ConversationBufferWindowMemory
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.runnables import RunnablePassthrough
